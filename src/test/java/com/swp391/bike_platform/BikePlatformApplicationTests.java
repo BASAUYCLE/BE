d91073f6db4b5cccf9cc37840bdcb44dd56047d1
Minori016 <xuhoadon@gmail.com>
2026-01-27 09:25:27 +0700

    feat(app): Add main application class and test setup

---
+++ src/test/java/com/swp391/bike_platform/BikePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swp391.bike_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
