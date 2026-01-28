d88e605f3d1e41677c4aa47cc746165bcf5cebc8
Minori016 <xuhoadon@gmail.com>
2026-01-28 20:36:30 +0700

    Add UserRegisterRequest DTO for user registration validation

---
+++ src/main/java/com/swp391/bike_platform/request/UserRegisterRequest.java
@@ -0,0 +1,14 @@
+package com.swp391.bike_platform.request;
+
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+public class UserRegisterRequest {
+    private String email;
+    private String password;
+    private String fullName;
+    private String phoneNumber;
+    private MultipartFile cccdFront;
+    private MultipartFile cccdBack;
+}
